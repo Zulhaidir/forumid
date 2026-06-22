6bcd0135b96c508cdd7451c6a56bbc9260dc558f
zulhaidir <zulhaidirm11@gmail.com>
2026-06-22 23:50:09 +0800

    Merapikan user_role

--- lib/forumid/access_control/user_role.ex
+++ lib/forumid/access_control/user_role.ex
@@ -1,25 +1,3 @@
-# defmodule Forumid.AccessControl.UserRole do
-#   use Ecto.Schema
-#   import Ecto.Changeset
-
-#   @primary_key {:id, :binary_id, autogenerate: true}
-#   @foreign_key_type :binary_id
-#   schema "user_roles" do
-
-#     field :user_id, :binary_id
-#     field :role_id, :binary_id
-
-#     timestamps(type: :utc_datetime)
-#   end
-
-#   @doc false
-#   def changeset(user_role, attrs) do
-#     user_role
-#     |> cast(attrs, [])
-#     |> validate_required([])
-#   end
-# end
-
 defmodule Forumid.AccessControl.UserRole do
   use Ecto.Schema
   import Ecto.Changeset
